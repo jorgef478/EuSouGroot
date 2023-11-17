12ca03eb75ed34786f7a1d754a92eea808b656d7
jorgef478 <jorgef478@gmail.com>
2023-11-17 19:47:09 -0300

    finalizado!

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+const string sair = "tchau";
+string pergunta;
+
+Console.WriteLine("~~~~ Bem vindo ao Groot Chatbot! ~~~~");
+
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+    string resposta = RespostaGroot(pergunta);
+    ExibeResposta(RespostaGroot(pergunta));
+}while (pergunta.Trim().ToLower() != sair);
+
+void ExibeResposta(string resposta)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"                          Resposta: {resposta}");
+    Console.ResetColor();
+}
+
+string RespostaGroot(string pergunta)
+{
+    return pergunta.Trim().ToLower() == sair ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
